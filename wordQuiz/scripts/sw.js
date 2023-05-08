f12dca5f3cd53050658e8a262468f92b9e877d18
aditya raj <aditya2400raj@gmail.com>
2023-05-08 20:10:34 +0530

    added word quiz

---
+++ wordQuiz/scripts/sw.js
@@ -0,0 +1,77 @@
+// Files to cache
+const cacheName = 'wq-v2';
+const contentToCache = [
+  '/index.html',
+  '/scripts/main.js',
+  '/lexis/a',
+  '/lexis/b',
+  '/lexis/c',
+  '/lexis/d',
+  '/lexis/e',
+  '/lexis/f',
+  '/lexis/g',
+  '/lexis/h',
+  '/lexis/i',
+  '/lexis/j',
+  '/lexis/k',
+  '/lexis/l',
+  '/lexis/m',
+  '/lexis/n',
+  '/lexis/o',
+  '/lexis/p',
+  '/lexis/q',
+  '/lexis/r',
+  '/lexis/s',
+  '/lexis/t',
+  '/lexis/u',
+  '/lexis/v',
+  '/lexis/w',
+  '/lexis/x',
+  '/lexis/y',
+  '/lexis/z'];
+
+// Installing Service Worker
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+  e.waitUntil((async () => {
+    const cache = await caches.open(cacheName);
+    console.log('[Service Worker] Caching all: app shell and content');
+    await cache.addAll(contentToCache);
+  })());
+});
+
+// Fetching content using Service Worker
+self.addEventListener('fetch', (e) => {
+    // Cache http and https only, skip unsupported chrome-extension:// and file://...
+    if (!(
+       e.request.url.startsWith('http:') || e.request.url.startsWith('https:')
+    )) {
+        return; 
+    }
+
+  e.respondWith((async () => {
+    const r = await caches.match(e.request);
+    console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+    if (r) return r;
+    const response = await fetch(e.request);
+    const cache = await caches.open(cacheName);
+    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+    cache.put(e.request, response.clone());
+    return response;
+  })());
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key === cacheName) {
+            return;
+          }
+          return caches.delete(key);
+        })
+      );
+    })
+  );
+});
